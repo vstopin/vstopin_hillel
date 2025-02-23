12d4dd4166d8d130f1dab9d7e59372f89ea936e4
vstopin <neostead12@gmail.com>
2025-02-23 17:01:17 +0200

    Added Math.min and Math.max methods, removed unnecessary code

--- js_homework6.js
+++ js_homework6.js
@@ -1,44 +1,29 @@
 var services = {
   "стрижка": "60 грн",
   "гоління": "80 грн",
-  "Миття голови": "100 грн",
-
-  addService: function(serviceName, price) {
-    this[serviceName] = price;
-  },
-
-  price: function() {
-    return Object.values(this)
-      .filter(value => typeof value === 'string')
-      .map(price => parseInt(price.replace(/\D/g, '')))
-      .reduce((total, price) => total + price, 0) + " грн";
-  },
-
-  minPrice: function() {
-    let min = Infinity;
-    for (let price of Object.values(this)) {
-      let numericPrice = typeof price === "string" ? parseInt(price.replace(/\D/g, '')) : price;
-      if (numericPrice < min) {
-        min = numericPrice;
-      }
-    }
-    return min + " грн";
-  },
-
-  maxPrice: function() {
-    let max = -Infinity;
-    for (let price of Object.values(this)) {
-      let numericPrice = typeof price === "string" ? parseInt(price.replace(/\D/g, '')) : price;
-      if (numericPrice > max) {
-        max = numericPrice;
-      }
-    }
-    return max + " грн";
-  }
+  "Миття голови": "100 грн"
 };
 
-services.addService('Розбити скло', '200 грн');
+services['Розбити скло'] = "200 грн";
 
-console.log("Загальна вартість послуг: " + services.price()); // 440 грн
-console.log("Мінімальна ціна: " + services.minPrice()); // 60 грн
-console.log("Максимальна ціна: " + services.maxPrice()); // 200 грн
+function price() {
+  let total = 0;
+  for (let service in services) {
+    total += parseInt(services[service]);
+  }
+  return total + " грн";
+}
+
+function minPrice() {
+  let prices = Object.values(services).map(price => parseInt(price));
+  return Math.min(...prices) + " грн";
+}
+
+function maxPrice() {
+  let prices = Object.values(services).map(price => parseInt(price));
+  return Math.max(...prices) + " грн";
+}
+
+console.log(price());
+console.log(minPrice()); 
+console.log(maxPrice());
